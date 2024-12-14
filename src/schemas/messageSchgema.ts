c5083a5531929d1ecad8f52c829f695d07a7f280
himanshu <himanshu.andel@yahoo.in>
2024-12-14 23:20:08 +0530

    Chai or code - Video 1 - Zod for validation in Nextjs

---
+++ src/schemas/messageSchgema.ts
@@ -0,0 +1,9 @@
+import { z } from "zod";
+
+export const messageSchema = z.object({
+  content: z.string().min(10, {
+    message: "Content must be atleast of 10 charaters"
+  }).max(300, {
+    message: "Content must be no longer than 300 charaters"
+  }),
+})
